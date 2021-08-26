b1539e83b565ffeb0e4d9203bb3fdf72daa7755f
Michael Dreisinger <mike.dreisinger95@gmail.com>
2021-08-26 16:28:21 -0600

    delete tests

--- helpers/delete_endpoint.py
+++ helpers/delete_endpoint.py
@@ -6,7 +6,7 @@ from typing import Tuple
 from helpers.base_endpoint import BaseEndpoint
 
 
-class GetEndpoint(BaseEndpoint):
+class DeleteEndpoint(BaseEndpoint):
     def __init__(self, 
                  server : str = "http://localhost",
                  port : str = "8083",
@@ -14,9 +14,9 @@ class GetEndpoint(BaseEndpoint):
                                   'Authorization': 'Basic YWRtaW46dGVzdFBhc3N3b3Jk'},
                  read_credentials : Tuple[str, str] = ("testUsername", "testPassword"),
                  write_credentials : Tuple[str, str] = ("admin", "testPassword"),
-                 endpoint : str = "/v1/get-person/"):
+                 endpoint : str = "/v1/delete-person/"):
         """!
-        @brief  A class which provides access to the Post Endpoint.
+        @brief  A class which provides access to the Delete Endpoint.
         @param  server The server address that the application is running on.
         @param  port The port that thte application is running on.
         @param  headers The headers to send with the request.
@@ -25,25 +25,25 @@ class GetEndpoint(BaseEndpoint):
         @param  endpoint The url extension that provides access to the endpoint.
         """
         super().__init__(server, port, headers)
-        self.get_url = self.url + endpoint
+        self.delete_url = self.url + endpoint
         self.read_credentials = read_credentials
         self.write_credentials = write_credentials
 
-    def get(self, id : int = 0, write_access = False):
+    def delete(self, id : int = 0, write_access = False):
         """!
-        @brief  get the data for the person with id from the endpoint.
-        @param  id the id of the person to get from the endpoint.
-        @param  write_access True if you want to do a get request with write access, 
-                             False if you want to do the get request with read access.
+        @brief  delete the data for the person with id from the endpoint.
+        @param  id the id of the person to delete from the endpoint.
+        @param  write_access True if you want to do a delete request with write access, 
+                             False if you want to do the delete request with read access.
         @return The request response.
         """
         if write_access:
-            return requests.request("GET", 
-                                    f"{self.get_url}{id}",
+            return requests.request("DELETE", 
+                                    f"{self.delete_url}{id}",
                                     headers=self.headers,
                                     auth=self.write_credentials)
 
-        return requests.request("GET", 
-                                f"{self.get_url}{id}",
+        return requests.request("DELETE", 
+                                f"{self.delete_url}{id}",
                                 headers=self.headers,
                                 auth=self.read_credentials)
