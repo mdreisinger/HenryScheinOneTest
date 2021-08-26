388ad1699707d7f8c014a34faba9d5a3a5b0f227
Michael Dreisinger <mike.dreisinger95@gmail.com>
2021-08-26 14:02:27 -0600

    make first post test pass

--- helpers/base_endpoint.py
+++ helpers/base_endpoint.py
@@ -2,12 +2,14 @@
 
 
 class BaseEndpoint:
-    def __init__(self, server : str, port : str):
+    def __init__(self, server : str, port : str, headers : str):
         """!
         @brief  a parent class for endpoint classes to inherit from.
         @param  server The server address that the application is running on, e.g. "http://localhost".
         @param  port The port that thte application is running on, e.g. "8083".
+        @param  headers The headers to send with the request.
         """
         self.server = server
         self.port = port
         self.url = server+":"+port
+        self.headers = headers
--- tests/post_endpoint_tests.py
+++ tests/post_endpoint_tests.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import json
+
 from helpers.database_actioner import DatabaseActioner
 from helpers.post_endpoint import PostEndpoint
 
 H2_JAR = "/home/mdreisinger/src/testAutomation/target/h2-1.4.200.jar"
 
-def test_postEndpointShouldReturn200GivenValidDataAndCredentials():
+def test_postEndpointShouldReturn201GivenValidDataAndCredentials():
     # Arrange
     #dba = DatabaseActioner(H2_JAR)
     post_endpoint = PostEndpoint()
@@ -16,7 +18,7 @@ def test_postEndpointShouldReturn200GivenValidDataAndCredentials():
     }
     
     # Act
-    actual_response = post_endpoint.post(data)
+    actual_response = post_endpoint.post(json.dumps(data))
     
     # Assert
-    assert actual_response.status_code == 200
+    assert actual_response.status_code == 201
